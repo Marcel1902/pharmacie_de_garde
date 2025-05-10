31b06fe863d793759be678e9a7c0aaf41d33d22b
Marcel1902 <faniloniainatanguymarcel@gmail.com>
2025-05-10 12:36:22 +0000

    push depuis pythonanywhere

--- pharmacie_de_garde/settings.py
+++ pharmacie_de_garde/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gsw^8u3#av665dyal@n0#9_-z+g4qa49^u0@n3_tw49e6n)z+2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Tanguy1902.pythonanywhere.com']
 
 
 # Application definition
